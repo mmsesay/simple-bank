89553006d3ddd624ad2c0ee82077afcf503d060e
mmsesay <maej_01@hotmail.com>
2021-12-24 17:05:27 +0000

    Update account name to account is schema-up Add: sqlc query file

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "account"(
+CREATE TABLE "accounts"(
     "id" BIGINT NOT NULL,
     "owner" VARCHAR(255) NOT NULL,
     "balance" BIGINT NOT NULL,
@@ -6,9 +6,9 @@ CREATE TABLE "account"(
     "created_at" TIMESTAMP(0) WITH TIME zone NOT NULL
 );
 CREATE INDEX "account_owner_index" ON
-    "account"("owner");
+    "accounts"("owner");
 ALTER TABLE
-    "account" ADD PRIMARY KEY("id");
+    "accounts" ADD PRIMARY KEY("id");
 CREATE TABLE "entries"(
     "id" INTEGER NOT NULL,
     "account_id" BIGINT NULL,
@@ -39,8 +39,8 @@ COMMENT
 ON COLUMN
     "transfers"."amount" IS 'must be positive';
 ALTER TABLE
-    "entries" ADD CONSTRAINT "entries_account_id_foreign" FOREIGN KEY("account_id") REFERENCES "account"("id");
+    "entries" ADD CONSTRAINT "entries_account_id_foreign" FOREIGN KEY("account_id") REFERENCES "accounts"("id");
 ALTER TABLE
-    "transfers" ADD CONSTRAINT "transfers_from_account_id_foreign" FOREIGN KEY("from_account_id") REFERENCES "account"("id");
+    "transfers" ADD CONSTRAINT "transfers_from_account_id_foreign" FOREIGN KEY("from_account_id") REFERENCES "accounts"("id");
 ALTER TABLE
-    "transfers" ADD CONSTRAINT "transfers_to_account_id_foreign" FOREIGN KEY("to_account_id") REFERENCES "account"("id");
+    "transfers" ADD CONSTRAINT "transfers_to_account_id_foreign" FOREIGN KEY("to_account_id") REFERENCES "accounts"("id");
